7074c318aa942ddbcba005a16326fe242b413da4
[AJ Davis] <78609746+ajdaviscodes@users.noreply.github.com>
2022-02-03 17:25:21 -0600

    Functionality
    game function built

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
-let firstCard = 10;
-let secondCard = 7;
-let sum = firstCard+secondCard;
-let hasBlackJack = false;
-if (sum <=20){
-    console.log("Do you want to draw a new card?")
-}else if(sum===21){
-    console.log("Wohoo! You've got Blackjack!")
-    hasBlackJack = true;
-}else{
-    console.log("You're out of the game!")
+let player = {
+    name: 'Player 1',
+    chips: 200
 }
-console.log(hasBlackJack);
+let cards = [];
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = false;
+let message = "";
+let messageEl = document.querySelector('#message-el');
+let sumEl = document.querySelector('#sum-el');
+let cardsEl = document.querySelector('#cards-el');
+let playerEl = document.querySelector('#player-el');
+
+playerEl.textContent = player.name + ': $' + player.chips
+
+
+function getRandomCard() {
+    return Math.floor(Math.random()*11)+1
+}
+
+function startGame(){
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard+secondCard;
+    isAlive = true;
+    renderGame();
+}
+
+
+function renderGame() {
+    cardsEl.textContent = 'Cards: |';
+    for(let i=0; i<cards.length; i++){
+        cardsEl.textContent += cards[i] + '|'
+    }
+
+    sumEl.textContent = `Sum: ${sum}`;
+    if (sum <= 20){
+        message = "Do you want to draw a new card?"
+    }else if(sum === 21){
+        message = "You've got Blackjack!"
+        hasBlackJack = true;
+    }else{
+        message = "BUST! You're out."
+        isAlive = false;
+    }
+
+   messageEl.textContent = message;
+
+}
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false){
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card)
+    renderGame();
+    }
+}
+
